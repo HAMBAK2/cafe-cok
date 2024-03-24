f0f8d7f2db82ef506cde0ffe7ebe296c32279cde
Hambak-note <dudghks5722@gmail.com>
2024-03-25 07:13:04 +0900

    infra(#3): dev 배포 자동화 테스트를 위한 커밋

--- src/main/java/com/sideproject/hororok/HororokApplication.java
+++ src/main/java/com/sideproject/hororok/HororokApplication.java
@@ -3,6 +3,7 @@ package com.sideproject.hororok;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class HororokApplication {
 
