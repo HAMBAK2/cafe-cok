fc76fbcff4f13f0fa023615aa4801d04248a9531
Hambak-note <dudghks5722@gmail.com>
2024-04-03 09:57:14 +0900

    refactor: 계획하기 결과로 반환되는 카페들에 대표 이미지를 추가

--- src/main/java/com/sideproject/hororok/cafe/dto/CafeDto.java
+++ src/main/java/com/sideproject/hororok/cafe/dto/CafeDto.java
@@ -1,14 +1,46 @@
 package com.sideproject.hororok.cafe.dto;
 
 import com.sideproject.hororok.cafe.entity.Cafe;
+import com.sideproject.hororok.cafeImage.entity.CafeImage;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
+@Builder
+@RequiredArgsConstructor
 public class CafeDto {
 
-    private final List<Cafe> cafes;
+    private final Long id;
+
+    private final String name;
+
+    private final String phoneNumber;
+
+    private final String roadAddress;
+    private final BigDecimal longitude; //경도
+    private final BigDecimal latitude; //위도
+    private final BigDecimal starRating;
+    private final Long reviewCount;
+    private final String image;
+
+    public static CafeDto from(Cafe cafe) {
+        return CafeDto.builder()
+                .id(cafe.getId())
+                .name(cafe.getName())
+                .phoneNumber(cafe.getPhoneNumber())
+                .roadAddress(cafe.getRoadAddress())
+                .longitude(cafe.getLongitude())
+                .latitude(cafe.getLatitude())
+                .starRating(cafe.getStarRating())
+                .reviewCount(cafe.getReviewCount())
+                .image(cafe.getImages().get(0).getImageUrl())
+                .build();
+    }
+
 }
