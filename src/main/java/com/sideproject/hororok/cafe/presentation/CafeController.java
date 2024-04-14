1a6353358e54a09ebb986df4aa710f45d51b7efa
Hambak-note <dudghks5722@gmail.com>
2024-04-15 04:54:20 +0900

    refactor(#69): cafe 관련 요청 URL을 변경
    - plans의 경우 cafe를 리팩토링 한 이후에 Plan이라는 엔티티를 작성한 후에 옮길 예정

--- src/main/java/com/sideproject/hororok/cafe/presentation/CafeController.java
+++ src/main/java/com/sideproject/hororok/cafe/presentation/CafeController.java
@@ -1,4 +1,4 @@
-package com.sideproject.hororok.cafe.controller;
+package com.sideproject.hororok.cafe.presentation;
 
 import com.sideproject.hororok.aop.annotation.LogTrace;
 import com.sideproject.hororok.cafe.cond.CafeCategorySearchCond;
@@ -7,37 +7,48 @@ import com.sideproject.hororok.cafe.cond.CreatePlanSearchCond;
 import com.sideproject.hororok.cafe.dto.*;
 import com.sideproject.hororok.cafe.service.CafePlanService;
 import com.sideproject.hororok.cafe.service.CafeService;
+import com.sideproject.hororok.category.dto.CategoryKeywords;
+import com.sideproject.hororok.category.service.CategoryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
-import java.time.LocalTime;
-import java.util.List;
 
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/cafe")
 @RequiredArgsConstructor
+@Tag(name = "Cafe", description = "카페 관련 API")
 public class CafeController {
 
     private final CafeService cafeService;
     private final CafePlanService cafePlanService;
+    private final CategoryService categoryService;
 
-    @GetMapping("/detail/{cafeId}")
-    @Operation(summary = "특정 지점에서 카페를 재검색 하는 기능")
-    @Parameter(description = "카페의 ID")
+    @GetMapping
+    @Operation(summary = "홈 화면에 보여줄 정보를 제공")
     @LogTrace
-    public ResponseEntity<CafeDetailDto> detail(@PathVariable Long cafeId){
+    public ResponseEntity<CategoryKeywords> home() {
 
+        return ResponseEntity.ok(categoryService.findAllCategoryAndKeyword());
+    }
+
+    @GetMapping("/{cafeId}")
+    @Operation(summary = "해당하는 카페의 상세 정보를 보여주는 기능")
+    @LogTrace
+    public ResponseEntity<CafeDetailDto> detail(
+            @Parameter(description = "카페의 ID")
+            @PathVariable Long cafeId){
         return ResponseEntity.ok(cafeService.findCafeDetail(cafeId));
     }
 
 
-    @GetMapping("/search/re")
+    @GetMapping("/find/re")
     @Operation(summary = "특정 지점에서 카페를 재검색 하는 기능")
     @Parameter(description = "현재 위치의 경도 위도 값")
     @LogTrace
@@ -47,7 +58,7 @@ public class CafeController {
         return ResponseEntity.ok(cafeService.findWithinRadius(CafeSearchCond.of(latitude, longitude)));
     }
 
-    @GetMapping("/search/bar")
+    @GetMapping("/find/bar")
     @Operation(summary = "검색창에 검색을 했을 때 동작하는 기능")
     @Parameter(description = "현재 위치의 경도 위도 값")
     @ApiResponse(description = "선택한 카페의 상세 정보를 전달, 카페가 존재하지 않는 경우 cafes, keywordsByCategory 정보 존재 나머지 X, \n카페가 존재하는 경우 반대 (exist 값은 항상 존재)")
@@ -58,7 +69,7 @@ public class CafeController {
         return ResponseEntity.ok(cafeService.barSearch(CafeSearchCond.of(latitude, longitude)));
     }
 
-    @PostMapping("/search/category")
+    @PostMapping("/find/category")
     @Operation(summary = "선택한 키워드와 현재 위치를 기준으로 검색")
     @Parameter(description = "현재 위치의 경도 위도 값, 선태한 키워드")
     @LogTrace
