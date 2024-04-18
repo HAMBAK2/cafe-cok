0682c2882a4801a67093ea8744a9b65515a7b0a5
Hambak-note <dudghks5722@gmail.com>
2024-04-18 18:06:17 +0900

    fix: 로그 추적기 관련 불필요 파일과 빌드 테스트 시 SpringApplicationTest의 존재로 실패 해서 삭제

--- src/main/java/com/sideproject/hororok/review/application/ReviewImageService.java
+++ src/main/java/com/sideproject/hororok/review/application/ReviewImageService.java
@@ -1,8 +1,6 @@
 package com.sideproject.hororok.review.application;
 
-import com.sideproject.hororok.aop.annotation.LogTrace;
 import com.sideproject.hororok.review.domain.ReviewImageRepository;
-import com.sideproject.hororok.review.dto.ReviewImageInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
