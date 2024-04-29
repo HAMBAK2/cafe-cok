d965ea83fcd27f1fe7296e3cb54cd88617c466ea
Hambak-note <dudghks5722@gmail.com>
2024-04-30 05:16:27 +0900

    test(#138): 카페 상세정보 상단 테스트 코드 구현 및 문서화

--- src/test/java/com/sideproject/hororok/common/fixtures/CafeFixtures.java
+++ src/test/java/com/sideproject/hororok/common/fixtures/CafeFixtures.java
@@ -4,12 +4,16 @@ import com.sideproject.hororok.bookmark.domain.BookmarkFolder;
 import com.sideproject.hororok.cafe.domain.Cafe;
 import com.sideproject.hororok.cafe.domain.CafeImage;
 import com.sideproject.hororok.cafe.dto.CafeDto;
+import com.sideproject.hororok.cafe.dto.response.CafeDetailTopResponse;
 
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+
+import static com.sideproject.hororok.common.fixtures.KeywordFixtures.키워드_DTO_리스트;
+import static com.sideproject.hororok.common.fixtures.ReviewFixtures.리뷰_개수;
 
 public class CafeFixtures {
 
@@ -35,6 +39,10 @@ public class CafeFixtures {
 
     public static List<CafeDto> 카페_DTO_리스트() {
         return Arrays.asList(카페_DTO());
+    }
+
+    public static CafeDetailTopResponse 카페_상세_상단_응답() {
+        return CafeDetailTopResponse.of(카페(), 리뷰_개수, 키워드_DTO_리스트());
     }
 
     public static BigDecimal getRandomBigDecimal(int min, int max) {
