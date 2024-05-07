b5e9dd1cece8c9f2fbb61308dbad01a345b4d328
Hambak-note <dudghks5722@gmail.com>
2024-05-07 21:02:13 +0900

    feat(#187): 관리자 페이지 카페 등록 API 구현

---
+++ src/main/java/com/sideproject/hororok/admin/domain/repository/CafeCopyRepository.java
@@ -0,0 +1,9 @@
+package com.sideproject.hororok.admin.domain.repository;
+
+import com.sideproject.hororok.admin.domain.CafeCopy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CafeCopyRepository extends JpaRepository<CafeCopy, Long> {
+
+
+}
---
+++ src/main/java/com/sideproject/hororok/admin/presentation/AdminController.java
@@ -0,0 +1,39 @@
+package com.sideproject.hororok.admin.presentation;
+
+import com.sideproject.hororok.admin.application.AdminService;
+import com.sideproject.hororok.admin.dto.request.AdminCafeSaveRequest;
+import com.sideproject.hororok.admin.dto.response.AdminCafeSaveResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+@Tag(name = "Admin", description = "관리자 페이지 관련 API")
+public class AdminController {
+
+    private final AdminService adminService;
+
+
+    @PostMapping(value = "/cafe/save",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "카페를 저장하는 기능")
+    public ResponseEntity<AdminCafeSaveResponse> saveCafe(
+            @RequestPart AdminCafeSaveRequest request,
+            @RequestPart(value = "mainImage") MultipartFile mainImage,
+            @RequestPart(value = "otherImages", required = false) List<MultipartFile> otherImages) {
+
+        AdminCafeSaveResponse response = adminService.saveCafe(request, mainImage, otherImages);
+        return ResponseEntity.ok(response);
+    }
+}
