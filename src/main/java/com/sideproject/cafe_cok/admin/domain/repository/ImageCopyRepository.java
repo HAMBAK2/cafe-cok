4076c72a48f65d36d56edce63492741989c96b0f
Hambak-note <dudghks5722@gmail.com>
2024-05-15 07:58:07 +0900

    remove: 임시로 사용하던 copy 엔티티 모두 삭제

--- src/main/java/com/sideproject/cafe_cok/admin/domain/repository/ImageCopyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sideproject.cafe_cok.admin.domain.repository;
-
-import com.sideproject.cafe_cok.admin.domain.ImageCopy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageCopyRepository extends JpaRepository<ImageCopy, Long> {
-
-}
--- src/main/java/com/sideproject/cafe_cok/admin/domain/repository/MenuCopyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sideproject.cafe_cok.admin.domain.repository;
-
-import com.sideproject.cafe_cok.admin.domain.MenuCopy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuCopyRepository extends JpaRepository<MenuCopy, Long>  {
-}
