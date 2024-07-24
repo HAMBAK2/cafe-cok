334736e965e122aba644509978a6b14e48812fed
Hambak-note <dudghks5722@gmail.com>
2024-07-24 21:04:20 +0900

    fix: 시큐리티 적용 후 발생하는 문제를 확인하기 위한 시큐리티 비활성화

--- src/main/java/com/sideproject/cafe_cok/CafeCokApplication.java
+++ src/main/java/com/sideproject/cafe_cok/CafeCokApplication.java
@@ -2,10 +2,12 @@ package com.sideproject.cafe_cok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class CafeCokApplication {
 
 	public static void main(String[] args) {
