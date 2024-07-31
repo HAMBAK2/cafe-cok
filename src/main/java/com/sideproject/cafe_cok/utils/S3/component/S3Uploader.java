966240066ab96acf42c10715b2ff309cb82fbea6
Hambak-note <dudghks5722@gmail.com>
2024-07-31 17:52:28 +0900

    refactor: 압축시 화질 문제로 압축 퀄리티 증가

--- src/main/java/com/sideproject/cafe_cok/utils/S3/component/S3Uploader.java
+++ src/main/java/com/sideproject/cafe_cok/utils/S3/component/S3Uploader.java
@@ -122,7 +122,7 @@ public class S3Uploader {
             ImageWriteParam param = writer.getDefaultWriteParam();
             if(param.canWriteCompressed()) {
                 param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-                param.setCompressionQuality(0.1f);
+                param.setCompressionQuality(0.5f);
             }
 
             writer.write(null, new IIOImage(image, null, null), param);
