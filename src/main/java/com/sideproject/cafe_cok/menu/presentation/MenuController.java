a612b6f53de17bc8def308ef7f324390ef58338d
y0unghwanhwang <dudghks5722@gmail.com>
2024-09-11 13:43:13 +0900

    docs(#306): 메뉴 관련 API(MENU) 명세 작성

--- src/main/java/com/sideproject/cafe_cok/menu/presentation/MenuController.java
+++ src/main/java/com/sideproject/cafe_cok/menu/presentation/MenuController.java
@@ -4,6 +4,9 @@ import com.sideproject.cafe_cok.cafe.presentation.CafeController;
 import com.sideproject.cafe_cok.menu.application.MenuService;
 import com.sideproject.cafe_cok.menu.dto.response.MenuIdResponse;
 import com.sideproject.cafe_cok.util.HttpHeadersUtil;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -18,12 +21,14 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RequestMapping("/api/v1/menus")
 @RequiredArgsConstructor
 @Tag(name = "menus", description = "메뉴 API")
+@ApiResponse(responseCode = "200", description = "성공")
 public class MenuController {
 
     private final MenuService menuService;
     private final HttpHeadersUtil httpHeadersUtil;
 
     @DeleteMapping("/{menuId}")
+    @Parameter(name = "menuId", description = "삭제하려는 메뉴 ID")
     public ResponseEntity<MenuIdResponse> delete(@PathVariable Long menuId) {
         MenuIdResponse response = menuService.delete(menuId);
         response.add(linkTo(methodOn(MenuController.class).delete(menuId)).withSelfRel().withType("DELETE"))
